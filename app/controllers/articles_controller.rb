77bef69dbaf32f5c4e917e19e8c3cf683fe9012b
山本将也 <0112masaya4849@gmail.com>
2023-10-25 21:32:19 +0900

    Task. 11-3 下書き記事の一覧/詳細 のAPI実装【下書き機能の実装】のコントローラーの実装の完了。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,45 +3,35 @@ module Api::V1
     before_action :authenticate_user!, only: [:create, :update, :destroy]
 
     def index
-      articles = Article.order(updated_at: :desc)
+      articles = Article.published.order(updated_at: :desc)
       render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
     end
 
     def show
-      #binding.pry
-      article = Article.find(params[:id])
+      article = Article.published.find(params[:id])
       render json: article, serializer: Api::V1::ArticleSerializer
     end
 
     def create
-      # binding.pry
       article = current_user.articles.create!(article_params)
       render json: article, serializer: Api::V1::ArticleSerializer
-
     end
 
     def update
-        article = current_user.articles.find(params[:id])
-        article.update!(article_params)
-        render json: article, serializer: Api::V1::ArticleSerializer
+      article = current_user.articles.find(params[:id])
+      article.update!(article_params)
+      render json: article, serializer: Api::V1::ArticleSerializer
     end
 
     def destroy
-        # 対象のレコードを探す
-        # binding.pry
-        article = current_user.articles.find(params[:id])
-        # 探してきたレコードを削除する
-        article.destroy!
-        # json として値を返す
-        render json: article, serializer: Api::V1::ArticleSerializer
+      article = current_user.articles.find(params[:id])
+      article.destroy!
     end
-
 
     private
 
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
-
+      def article_params
+        params.require(:article).permit(:title, :body, :status)
+      end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,16 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :articles do
+        resources :drafts
+      end
       mount_devise_token_auth_for "User", at: "auth", controllers: {
         registrations: "api/v1/auth/registrations",
       }
       resources :articles
+
+
+
     end
   end
 end
