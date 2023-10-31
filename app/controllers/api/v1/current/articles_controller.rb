e9d8c9df2ebaa97b15238280351d7cfcf1b75165
山本将也 <0112masaya4849@gmail.com>
2023-10-31 20:25:17 +0900

    Task.13 フロントエンドとの繋ぎこみ part.3の完了

--- app/controllers/api/v1/current/articles_controller.rb
+++ app/controllers/api/v1/current/articles_controller.rb
@@ -1,12 +1,10 @@
-class Api::V1::Current::ArticlesController < Api::V1::BaseApiController
-  before_action :authenticate_user!, only: :api_index
-  def index
-    # binding.pry
-    @articles = Article.all
-    articles = @articles
-    @myarticles = current_user.articles.published.order(updated_at: :desc)
-    # articles = Article.articles.published.order(updated_at: :desc)
-    render json: @myarticles, each_serializer: Api::V1::ArticlePreviewSerializer
-  end
+module Api::V1
+  class Current::ArticlesController < BaseApiController
+    before_action :authenticate_user!
 
+    def index
+      articles = current_user.articles.published.order(updated_at: :desc)
+      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
+    end
+  end
 end
